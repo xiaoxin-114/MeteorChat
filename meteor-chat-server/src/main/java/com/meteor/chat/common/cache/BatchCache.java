54c239468ace4176ef2f427e29bb0c3f39ce86df
liuxin <1234@qq.com>
2024-04-12 17:58:16 +0800

    用户信息缓存补充

---
+++ meteor-chat-server/src/main/java/com/meteor/chat/common/cache/BatchCache.java
@@ -0,0 +1,38 @@
+package com.meteor.chat.common.cache;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 批量缓存模型
+ * @param <K>
+ * @param <V>
+ */
+public interface BatchCache<K, V> {
+
+    /**
+     * 使用旁路缓存获取单个值
+     * @param k
+     * @return
+     */
+    V get(K k);
+    /**
+     * 使用旁路缓存批量获取多个值
+     * @param list
+     * @return
+     */
+    Map<K, V> getBatch(List<K> list);
+    /**
+     * 删除缓存中单个值
+     * @param k
+     * @return
+     */
+    void delete(K k);
+    /**
+     * 批量删除缓存中的值
+     * @param list
+     * @return
+     */
+    void deleteBatch(List<K> list);
+
+}
