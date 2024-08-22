5cad87f25c35082d192eb3bb7f49e1d1a6604f49
liuxin <1234@qq.com>
2024-08-22 14:06:27 +0800

    跨域响应头调整

--- meteor-chat-server/src/main/java/com/meteor/chat/common/interceptor/CorsInterceptor.java
+++ meteor-chat-server/src/main/java/com/meteor/chat/common/interceptor/CorsInterceptor.java
@@ -1,20 +1,21 @@
 package com.meteor.chat.common.interceptor;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class CorsInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Method", "'GET, POST, OPTIONS, DELETE, PUT'");
-        response.addHeader("Access-Control-Allow-Header", "'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization'");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
+        response.addHeader("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization");
         response.addHeader("Access-Control-Max-Age", "1728000");
-        if (request.getMethod().equals("OPTION")) {
+        if (request.getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_NO_CONTENT);
             return false;
         }
